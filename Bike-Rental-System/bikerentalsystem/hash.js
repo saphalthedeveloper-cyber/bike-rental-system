7a4778af3c940ba786c5a322b715e9193cd29822
saphalsingh2@gmail.com <saphalsingh07@gmail.com>
2026-07-21 22:20:42 +0545

    Remove duplicate nested project folder

--- Bike-Rental-System/bikerentalsystem/hash.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require('bcrypt');
-
-async function hashPassword() {
-    const salt = await bcrypt.genSalt();
-    const hash = await bcrypt.hash('owner1234', salt);
-    console.log(hash);
-}
-
-hashPassword();
