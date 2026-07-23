0d5fb8382120b2344bce3a21f0f8c9c9b944afb1
saphalsingh2@gmail.com <saphalsingh07@gmail.com>
2026-07-23 19:32:38 +0545

    fix case-sensitive import

--- src/Booking.jsx
+++ src/Booking.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
-import useFetch from './UseFetch'
+import useFetch from './useFetch'
 
 const Booking = () => {
   const { bikeId } = useParams()
